e654b9c4d527dbf806d37c64a09c05530dfb0091
traynhamdavid426 <traynhamdavid426@gmail.com>
2023-05-03 23:51:05 -0400

    SIgn in/up pages updated and changes made to App

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,23 +1,32 @@
+import { keyboard } from "@testing-library/user-event/dist/keyboard";
 import React,{useState} from "react";
 
 
 
-const Signup =() =>{
-    const [email,setEmail] = useState('');
-    const [password,setPassword] = useState('');
-    const [name,setName] = useState('');
-    
-    return(<div>
-    <form onSubmit={handleInput}>
-   <div>  <label for='email:'>email</label>
-<input type="email" value={email} placeholder="youremail@emailprovider.com"/></div>
+const Signup =(props) =>{
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [name, setName] = useState('');
 
-<div>  <label for='password:'>password</label>
-<input type="password" value={password} placeholder="*****"/></div>
-<div><button type="submit">
-Log in
-</button></div>
- </form>
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email); //prevents page reloading.
+    }
+
+    return (
+        <div >
+            <h2>Sign up</h2>
+        <form  onSubmit={handleSubmit}>
+            <label htmlFor="name">Full name</label>
+            <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full Name" />
+            <label htmlFor="email">email</label>
+            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+            <label htmlFor="password">password</label>
+            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+            <button type="submit">Sign Up</button>
+        </form>
+        <button className="link-btn" onClick={() => props.onFormSwitch('signin')}>Already have an account? Login here.</button>
 </div>)}
 
 export default Signup;
+
