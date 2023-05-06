6e3293c14ef58cc8e8437ff7fab59100d82d3e0e
traynhamdavid426 <traynhamdavid426@gmail.com>
2023-05-06 16:40:50 -0400

    New updates

---
+++ src/Components/Groupedcomponents/Schedules.js
@@ -0,0 +1,12 @@
+import React from "react";
+import DynamicSchedule from "../DynamicSchedule";
+import Schedule from '../Schedule'
+
+const Schedules =() => {return(<div>
+
+<DynamicSchedule/>
+<Schedule/>
+
+</div>)}
+
+export default Schedules
